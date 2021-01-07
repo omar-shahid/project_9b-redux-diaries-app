0e8f13b3b02e1fedda19a884c5eb56c4a48b0e3f
Omar Shahid <omarshahid160@gmail.com>
2021-01-07 11:52:56 +0500

    Setop mocking server

---
+++ src/app/server.js
@@ -0,0 +1,17 @@
+import { createServer } from "miragejs"
+
+function startServer() {
+  createServer({
+    routes() {
+      this.get("/api/reminders", () => ({
+        reminders: [
+            { id: 1, text: "Walk the dog" },
+            { id: 2, text: "Take out the trash" },
+            { id: 3, text: "Work out" },
+        ],
+      }))
+    },
+  })
+}
+
+export default startServer
